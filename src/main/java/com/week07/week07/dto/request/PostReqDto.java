1bfc29de1c48f5e3a483655746b5047776540185
meruberu <wmj0924@gmail.com>
2022-10-29 12:15:47 +0900

    Post CRUD 1차, Tag 임시 추가

---
+++ src/main/java/com/week07/week07/dto/request/PostReqDto.java
@@ -0,0 +1,23 @@
+package com.week07.week07.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Lob;
+import java.util.List;
+
+
+@Getter
+@NoArgsConstructor
+public class PostReqDto {
+
+
+    private String title;
+
+    @Lob
+    private String imgUrl;
+
+    private String content;
+
+    private String tag;
+}
