d12ec973d4d7f3d933e8197563fd86d5349b39e3
meruberu <wmj0924@gmail.com>
2022-10-29 12:15:47 +0900

    Post CRUD 1차, Tag 임시 추가

---
+++ src/main/java/com/week07/week07/domain/Tag.java
@@ -0,0 +1,30 @@
+package com.week07.week07.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long tagId;
+
+    private String tag;
+
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+
+
+
+}
